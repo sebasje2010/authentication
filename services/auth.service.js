74bcd1a7a63ee3e868742d0eefa72c79cf9c9763
Sebastian Jativa <sebastian.jativa@hotmail.com>
2022-08-01 18:27:48 -0500

    validado token de cambio de contrasena

--- services/auth.service.js
+++ services/auth.service.js
@@ -50,6 +50,21 @@ class AuthService {
         return rta
     }
 
+    async changePassword(token,newPassword){
+        try{
+            const payload=jwt.verify(token,config.jwtSecret)
+            const user=await service.findOne(payload.sub)
+            if(user.recoveryToken!==token){
+                throw boom.unauthorized('Invalid token')
+            }
+            const hashedPassword=await bcrypt.hash(newPassword,10)
+            await service.update(user.id,{password:hashedPassword,recoveryToken:null})
+            return {message:'Password changed'}
+        } catch(error){
+            throw boom.unauthorized('Invalid token')
+        }
+    }
+
     async sendEmail(infoMail){
         const transporter = nodemailer.createTransport({
             secure: true, // true for 465, false for other ports
